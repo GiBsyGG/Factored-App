9850218289d3087aa3aefb00b2eb356a0c720dd0
GiBsyGG <jcval2801@gmail.com>
2024-06-03 04:22:55 -0500

    Add: Form Error Handling

--- Factored_Frontend/factored_app/app/components/loginForm/LoginForm.tsx
+++ Factored_Frontend/factored_app/app/components/loginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import TextField from '@mui/material/TextField'
 import { cyan } from '@mui/material/colors'
@@ -19,46 +19,56 @@ export function LoginForm() {
 
   const router = useRouter()
 
+  const [errorInput, setErrorInput] = useState(false)
+  const [errorText, setErrorText] = useState('')
+
+  const [errorPassword, setErrorPassword] = useState(false)
+  const [errorPasswordText, setErrorPasswordText] = useState('')
+
   const FormSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
   
     const formData = new FormData(event.currentTarget)
-    // Probably not a secure way to send the data with password
-    const response = await axios.post('http://127.0.0.1:8000/auth/login', {
-      email: formData.get('email'),
-      password: formData.get('password')
-    })
+    try {
+      // Probably not a secure way to send the data with password
+      const response = await axios.post('http://127.0.0.1:8000/auth/login', {
+        email: formData.get('email'),
+        password: formData.get('password')
+      })
 
+      if (response.status === 200) {
+        const data = response.data
+        const token = data.token
+        const employee_id: number = data.employee_id
     
-  
-    if (response.status === 200) {
-      const data = response.data
-      const token = data.token
-      const employee_id: number = data.employee_id
-  
-      // Save the token and employee as cookie
-      setCookie('accessToken', token, {
-        maxAge: 60 * 60, // 1 hour
-        path: '/',
-      })
-  
-      setCookie('employee_id', employee_id.toString(), {
-        maxAge: 60 * 60, // 1 hour
-        path: '/',
-      })
-  
-      // Redirect to the profile page with the employee id
-      router.push(`/employees/${employee_id}`)
-    } else {
-      notFound();
+        // Save the token and employee as cookie
+        setCookie('accessToken', token, {
+          maxAge: 60 * 60, // 1 hour
+          path: '/',
+        })
+    
+        setCookie('employee_id', employee_id.toString(), {
+          maxAge: 60 * 60, // 1 hour
+          path: '/',
+        })
+
+        router.push(`/employees/${employee_id}`)
+      }
+    } catch (error: any) {
+        if (error.response.status === 404) {
+          setErrorInput(true)
+          setErrorText('Invalid email')
+        } else if (error.response.status === 401) {
+          setErrorPassword(true)
+          setErrorPasswordText('Wrong password')
+        }
     }
-    
   }
 
   return (
     <form onSubmit={FormSubmitHandler} className={style.form_container}>
-      <TextField id="email" name="email" label="Email" type='email' variant="outlined" size='medium' fullWidth required />
-      <TextField id="password" name="password" label="Password" type='password' variant="outlined" size='medium' fullWidth required />
+      <TextField id="email" name="email" label="Email" type='email' variant="outlined" size='medium' fullWidth required error={errorInput} helperText={errorText} />
+      <TextField id="password" name="password" label="Password" type='password' variant="outlined" size='medium' fullWidth required error={errorPassword} helperText={errorPasswordText} />
       <Button  
         variant="contained" 
         sx={{bgcolor: "#42B6C5", "&:hover": {bgcolor: cyan[800]}}}
--- Factored_Frontend/factored_app/app/components/registerForm/RegisterForm.tsx
+++ Factored_Frontend/factored_app/app/components/registerForm/RegisterForm.tsx
@@ -35,6 +35,9 @@ export function RegisterForm() {
 
   const router = useRouter()
 
+  const [errorInput, setErrorInput] = useState(false)
+  const [errorText, setErrorText] = useState('')
+
   const skills = [1, 2, 3, 4, 5]
 
   const FormSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -42,59 +45,63 @@ export function RegisterForm() {
 
     const formData = new FormData(event.currentTarget)
 
-    // Create a dictionary of the skills and their values
-    let skills_dict: { [key: string]: string } = {} // Add type annotation for skills_dict
+    try{
+      // Create a dictionary of the skills and their values
+      let skills_dict: { [key: string]: string } = {} // Add type annotation for skills_dict
 
-    for (let i = 1; i <= 5; i++) {
-      const skill = formData.get(`skill${i}`)?.toString()
-      const value = formData.get(`value ${i}`)?.toString() // Convert the value to a string
+      for (let i = 1; i <= 5; i++) {
+        const skill = formData.get(`skill${i}`)?.toString()
+        const value = formData.get(`value ${i}`)?.toString() // Convert the value to a string
+        
+        if (skill && value) {
+          skills_dict[skill] = value
+        }
+      }
       
-      if (skill && value) {
-        skills_dict[skill] = value
+      // Probably not a secure way to send the data with password
+      const response = await axios.post('http://127.0.0.1:8000/auth/register', {
+        email: formData.get('email'),
+        hashed_password: formData.get('password'),
+        name: formData.get('name'),
+        position: formData.get('CompanyPosition'),
+        linkedin_url: formData.get('linkedinURL'),
+        about_employee: formData.get('bio'),
+        skills: skills_dict
+      })
+      
+    
+      if (response.status === 201) {
+        const data = response.data
+        const token = data.token
+        const employee_id: number = data.employee_id
+    
+        // Save the token and employee as cookie
+        setCookie('accessToken', token, {
+          maxAge: 60 * 60, // 1 hour
+          path: '/',
+        })
+    
+        setCookie('employee_id', employee_id.toString(), {
+          maxAge: 60 * 60, // 1 hour
+          path: '/',
+        })
+    
+        // Redirect to the profile page with the employee id
+        router.push(`/employees/${employee_id}`)
+    }
+    } catch (error: any) {
+      if (error.response.status === 409) {
+        setErrorInput(true)
+        setErrorText('User already exists')
       }
     }
-    
-    // Probably not a secure way to send the data with password
-    const response = await axios.post('http://127.0.0.1:8000/auth/register', {
-      email: formData.get('email'),
-      hashed_password: formData.get('password'),
-      name: formData.get('name'),
-      position: formData.get('CompanyPosition'),
-      linkedin_url: formData.get('linkedinURL'),
-      about_employee: formData.get('bio'),
-      skills: skills_dict
-    })
-    
-  
-    if (response.status === 201) {
-      const data = response.data
-      const token = data.token
-      const employee_id: number = data.employee_id
-  
-      // Save the token and employee as cookie
-      setCookie('accessToken', token, {
-        maxAge: 60 * 60, // 1 hour
-        path: '/',
-      })
-  
-      setCookie('employee_id', employee_id.toString(), {
-        maxAge: 60 * 60, // 1 hour
-        path: '/',
-      })
-  
-      // Redirect to the profile page with the employee id
-      router.push(`/employees/${employee_id}`)
-    } else {
-      notFound();
-    }
-    
   }
 
   return (
 
     <form onSubmit={FormSubmitHandler} className={style.form_container}>
       <TextField id="name" name="name" label="Name" type='text' variant="outlined" size='medium' fullWidth required />
-      <TextField id="email" name="email" label="Email" type='email' variant="outlined" size='medium' fullWidth required />
+      <TextField id="email" name="email" label="Email" type='email' variant="outlined" size='medium' fullWidth required error={errorInput} helperText={errorText} />
       <TextField id="CompanyPosition" name="CompanyPosition" label="Company Position" type='text' variant="outlined" size='medium' fullWidth required />
       <TextField id="linkedinURL" name="linkedinURL" label="LinkedinURL" type='url' variant="outlined" size='medium' fullWidth />
       <textarea id="bio" name="bio" placeholder="About Employee" className={style.bio}/>
