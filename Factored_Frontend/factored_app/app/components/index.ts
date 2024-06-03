85ad46967f0b7f9d9089fa810603d9083b518ed2
GiBsyGG <jcval2801@gmail.com>
2024-06-03 17:13:41 -0500

    Update index.ts

--- Factored_Frontend/factored_app/app/components/index.ts
+++ Factored_Frontend/factored_app/app/components/index.ts
@@ -1,5 +1,5 @@
 export { LogoutButton } from './logoutButton/LogoutButton';
 export { LoginForm } from './loginForm/LoginForm';
 export { Sidebar } from './sidebar/Sidebar';
-export { SidebarItem } from './sidebarItem/SidebarItem';
+export { SidebarItem } from './SidebarItem/SidebarItem';
 export { RegisterForm } from './registerForm/RegisterForm';
